4f84fc8a0ca88c40669e2da45b76002be109f67f
MD Parves <parvesmd270@gmail.com>
2025-07-04 12:50:39 +0000

    Error: Invalid src prop (https://img.drz.lazcdn.com/g/kf/S59be463de0cc4c

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.drz.lazcdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
