40c7d053a71ef3975b55b112a6f0b95634e9ee48
MD Parves <parvesmd270@gmail.com>
2025-07-05 19:12:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lzd-img-global.slatic.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
